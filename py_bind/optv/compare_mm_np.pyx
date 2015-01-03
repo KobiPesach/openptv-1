e2e3a8408105b3f414f3cb591b3e0c36686374cb
KobiPesach <kobipes@gmail.com>
2015-01-03 23:33:57 +0200

    Update compare_mm_np.pyx

--- py_bind/optv/compare_mm_np.pyx
+++ py_bind/optv/compare_mm_np.pyx
@@ -1,15 +1,2 @@
-def _compare_mm_np_(mm1, mm2) :
- 	"""
-	compare_mm_np_() checks that all fields of two mm_np objects are
-	equal.
-	Arguments:
-	mm_np *mm1, *mm2 - addresses of the objects for comparison.
-	Returns:
-	True if equal, false otherwise.
-	"""
-
-	if ((mm1.nlay == mm2.nlay) and (mm1.n1 == mm2.n1)  and (mm1.n2[3] == mm2.n2[3]) and
-		(mm1.d[3] == mm2.d[3]) and (mm1.n3 == mm2.n3) and (mm1.lut == mm2.lut)) :
-			return 1
-	else
-			return 0
+cdef extern from "openptv-1/libopt/include/parameters.h"
+	int compare_mm_np(mm_np *mm1,mm_np *mm2) :
